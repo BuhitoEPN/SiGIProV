875bf4c62f11bd5008924943ea8a9ff271018523
DOMENICA CAMILA JIMENEZ SUPE <domenica.jimenez@epn.edu.ec>
2020-01-18 13:40:07 -0500

    feature: Conection folder

---
+++ SiGIProV/SiGIProV/Conexion/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SiGIProV.Conexion
+{
+    public abstract class Conexion
+    {
+        private readonly string connectionString;
+        public Conexion() => connectionString = "Server=.\\SQLEXPRESS;DataBase=RuescoCentral; integrated security = true";
+        protected SqlConnection GetConexion()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
